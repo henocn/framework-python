447addbe7edda6983d83802950419d401c6a95a9
Henoc N'GASAMA <ngasamah@gmail.com>
2025-08-05 13:13:03 +0000

    fix: Config of fiveicon with another app's method

--- api/api.py
+++ api/api.py
@@ -4,6 +4,13 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+
+@app.route('/favicon.ico', methods=['GET'])
+def favicon():
+    return app.send_static_file('favicon.ico')
+
+
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distance reading of size fiction novels</p>"
