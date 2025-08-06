1a92b56d126fd44a678e7d94192414daf4f86e05
Henoc N'GASAMA <ngasamah@gmail.com>
2025-08-06 14:14:11 +0000

    Mise en place d'une api avec sqlite

--- api/api.py
+++ api/api.py
@@ -1,18 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models.Book import Book, db
 
 app = Flask(__name__)
-
-app.config['DEBUG'] = True
-
-
-@app.route('/favicon.ico', methods=['GET'])
-def favicon():
-    return app.send_static_file('favicon.ico')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db.init_app(app)
 
 
+with app.app_context():
+    db.create_all()
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distance reading of size fiction novels</p>"
 
-app.run(port=5000)
+@app.route('/books', methods=['GET'])
+def get_books():
+    books = Book.query.all()
+    return jsonify([book.to_dict() for book in books])
+
+
+
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return jsonify(book.to_dict())
+
+
+
+@app.route('/books', methods=['POST'])
+def create_book():
+    data = request.get_json()
+    title = data.get('title')
+    author = data.get('author')
+
+    if not title or not author:
+        return jsonify({"error": "Title and author are required"}), 400
+
+    new_book = Book(title=title, author=author)
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify(new_book.to_dict()), 201
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ api/models/Book.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    author = db.Column(db.String(120), nullable=False)
+    year_published = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+    def to_dict(self):
+        return {"id": self.id, "title": self.title, "author": self.author, "year_published": self.year_published}
